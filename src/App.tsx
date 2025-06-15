a303ab863d5ffefb757a203d9fe555253a94f550
manoharbetha <manoharbetha.mb@gmail.com>
2025-06-15 13:41:10 +0000

    Reverted to commit e4dfb15751fbfaa65eff60f215d9234559237638

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -6,7 +7,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Auth from "./pages/Auth";
-import Landing from "./pages/Landing";
 
 const queryClient = new QueryClient();
 
@@ -17,8 +17,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/dashboard" element={<Index />} />
+          <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
